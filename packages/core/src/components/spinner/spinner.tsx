7f0d96b61450a5383366c61f62a58d04e09465a7
Masood Abdolian <m.abdolian@gmail.com>
2021-02-25 17:25:09 +0330

    fixed transition names

--- packages/core/src/components/spinner/spinner.tsx
+++ packages/core/src/components/spinner/spinner.tsx
@@ -3,7 +3,7 @@ import { GlobalConfig } from '@app/services';
 import { SpinnerSize, SpinnerType } from './spinner.types';
 
 /**
- * It's a circular indicator of progress and activity in order to show the loading state of a component or page when the user needs to wait on.
+ * It's an indicator of progress and activity in order to show the loading state of a component or page when the user needs to wait on.
  * @examples default, size, custom-size, color, type, customize
  */
 @Component({
