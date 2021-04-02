724bd5e3aace533f506faab466de93922b59c42c
Masood Abdolian <m.abdolian@gmail.com>
2021-04-02 19:54:00 +0430

    add template component

---
+++ packages/core/src/components/template/template.tsx
@@ -0,0 +1,54 @@
+import { Component, Host, Prop, h } from '@stencil/core';
+import { GlobalConfig } from '@app/services';
+// import { } from './toast.types';
+
+/**
+ * TODO
+ * @internal 
+ * @slot - The default slot
+ * @examples default
+ */
+@Component({
+  tag: 'plus-template',
+  styleUrl: 'template.scss',
+  shadow: true
+})
+export class Template {
+
+  /**
+   * TODO
+   */
+  @Prop()
+  disabled?: boolean;
+
+  @GlobalConfig('template')
+  config?;
+
+  /**
+   * External Methods
+   */
+
+  /**
+   * Internal Methods
+   */
+
+  /**
+   * Watchers
+   */
+
+  /**
+   * Events handler
+   */
+
+  /**
+   * Lifecycles
+   */
+
+  render() {
+    return (
+      <Host>
+        <slot />
+      </Host>
+    )
+  }
+}
