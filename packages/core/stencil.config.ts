88d3e8d451cdab3f54ef656f4963e55dd04bb34c
Masood Abdolian <masood.abdolian@rdsysco.com>
2021-04-06 15:12:04 +0430

    clean

--- packages/core/stencil.config.ts
+++ packages/core/stencil.config.ts
@@ -46,6 +46,7 @@ export const config: Config = {
     { components: ['plus-aspect-ratio'] },
     { components: ['plus-card', 'plus-card-body', 'plus-card-footer', 'plus-card-header'] },
     { components: ['plus-click-outside'] },
+    { components: ['plus-cropper'] },
     { components: ['plus-dialog', 'plus-dialog-body', 'plus-dialog-content', 'plus-dialog-footer', 'plus-dialog-header', 'plus-dialog-toggler'] },
     { components: ['plus-divider'] },
     { components: ['plus-drawer', 'plus-drawer-toggler'] },
@@ -53,8 +54,10 @@ export const config: Config = {
     { components: ['plus-intersection'] },
     { components: ['plus-layout'] },
     { components: ['plus-spinner'] },
+    { components: ['plus-sticky'] },
     { components: ['plus-switch'] },
     { components: ['plus-tabs', 'plus-tabs-bar', 'plus-tabs-panel', 'plus-tabs-panels', 'plus-tabs-tab'] },
+    { components: ['plus-tooltip'] },
     { components: ['plus-transition'] },
     { components: ['plus-tunnel-consumer'] },
   ]
