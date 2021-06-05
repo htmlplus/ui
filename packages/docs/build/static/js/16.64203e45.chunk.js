1e158b919ebeff09233f1899f6c59c319808d14f
Amir Tavakoli <amirtavakoli@outlook.com>
2021-06-05 10:56:41 +0430

    remove docs from packages

--- packages/docs/build/static/js/16.64203e45.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@htmlplus/docs"]=this["webpackJsonp@htmlplus/docs"]||[]).push([[16],{119:function(t,e,s){"use strict";s.r(e);var n=s(0),c=s.n(n),a=s(3);e.default=function(){return c.a.createElement("div",null,c.a.createElement(a.b.Link,{to:"ROUTE:COMPONENT:DETAILS",state:{key:"switch"}},"GO TO UI COMPONENTS"))}}}]);
-//# sourceMappingURL=16.64203e45.chunk.js.map
