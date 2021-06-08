d5c10cdf5536c40ed9126ddcf0f20e8ff14dbe14
Masood Abdolian <masood.abdolian@rdsysco.com>
2021-06-08 14:57:07 +0430

    update

--- packages/playground/react/src/port/port.tsx
+++ packages/playground/react/src/port/port.tsx
@@ -1,48 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Dialog as PlusDialog } from '@htmlplus/react';
 import type { JSX } from '@htmlplus/core';
+import { proxy } from './proxy';
 
-const dashToPascalCase = (str: string) => {
-  return str
-    .toLowerCase()
-    .split('-')
-    .map((segment) => segment.charAt(0).toUpperCase() + segment.slice(1))
-    .join('');
-}
-
-interface HTMLStencilElement extends HTMLElement {
-  componentOnReady(): Promise<this>;
-}
-
-interface StencilReactInternalProps<ElementType> extends React.HTMLAttributes<ElementType> {
-  forwardedRef: React.RefObject<ElementType>;
-  ref?: React.Ref<any>;
-}
-
-const proxy = <PropType, ElementType /*extends HTMLStencilElement*/>(tagName: string) => {
-
-  const displayName = dashToPascalCase(tagName);
-
-  const C: React.FC<any> = (props) => {
-
-    const { children, ...a } = props;
-
-    const newProps = { ...a };
-
-    useEffect(() => {
-
-      return () => {
-
-      }
-    }, []);
-
-    return React.createElement(tagName, newProps, children);
-  }
-
-  return C;
-}
-
-const Dialog = proxy<JSX.PlusDialog, HTMLPlusDialogElement>('plus-dialog');
+const Dialog = proxy<JSX.PlusDialog, HTMLPlusDialogElement>('plus-dialog', ['onPlusClose', 'onPlusClosed', 'onPlusOpen', 'onPlusOpened']);
 
 const Port = () => {
 
@@ -52,7 +13,7 @@ const Port = () => {
     <button onClick={() => setOpen(true)}>
       open
     </button>
-    <Dialog open={open}>
+    <Dialog open={open} onPlusClosed={() => setOpen(false)}>
       <div>
         ssssssssss
       </div>
