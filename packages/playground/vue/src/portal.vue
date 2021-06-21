4f6abb8e695cf566f77a6c33e3a43e092f323b49
Masood Abdolian <m.abdolian@gmail.com>
2021-06-21 19:54:01 +0430

    clean

--- packages/playground/vue/src/portal.vue
+++ packages/playground/vue/src/portal.vue
@@ -1,46 +1,46 @@
 <template>
   <div>
     <div class="center">
-      <PlusDialogToggler connector="dialog-animation"> Open </PlusDialogToggler>
+      <DialogToggler connector="dialog-animation"> Open </DialogToggler>
     </div>
-    <PlusDialog portal animation="fade" connector="dialog-animation">
-      <PlusDialogContent>
-        <PlusDialogHeader> Dialog Title </PlusDialogHeader>
-        <PlusDialogBody>
+    <Dialog portal animation="fade" connector="dialog-animation">
+      <DialogContent>
+        <DialogHeader> Dialog Title </DialogHeader>
+        <DialogBody>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
           aliquip ex ea commodo consequat.
-        </PlusDialogBody>
-        <PlusDialogFooter>
+        </DialogBody>
+        <DialogFooter>
           <button @click="increase">
             Clicked {{ count }} {{ count === 1 ? "time" : "times" }}
           </button>
-          <PlusDialogToggler> Close </PlusDialogToggler>
-        </PlusDialogFooter>
-      </PlusDialogContent>
-    </PlusDialog>
+          <DialogToggler> Close </DialogToggler>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   </div>
 </template>
 
 <script>
 import {
-  PlusDialog,
-  PlusDialogBody,
-  PlusDialogContent,
-  PlusDialogFooter,
-  PlusDialogHeader,
-  PlusDialogToggler,
-} from "@htmlplus/vue-original";
+  Dialog,
+  DialogBody,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogToggler,
+} from "@htmlplus/vue";
 
 export default {
   components: {
-    PlusDialog,
-    PlusDialogBody,
-    PlusDialogContent,
-    PlusDialogFooter,
-    PlusDialogHeader,
-    PlusDialogToggler,
+    Dialog,
+    DialogBody,
+    DialogContent,
+    DialogFooter,
+    DialogHeader,
+    DialogToggler,
   },
   data() {
     return {
