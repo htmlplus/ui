1929b602bb24df112bd322f3f10c05aa479b698d
Masood Abdolian <m.abdolian@gmail.com>
2021-09-12 23:39:23 +0430

    merge

--- packages/core.new/rollup.config.js
+++ packages/core.new/rollup.config.js
@@ -2,35 +2,51 @@ import resolve from 'rollup-plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import commonjs from 'rollup-plugin-commonjs';
 // import { terser } from 'rollup-plugin-terser';
-import { HTMLPLUS } from './transformer/rollup.plugin';
-import { HTMLPLUSDOCS } from './transformer/rollup.plugin.docs';
+import { htmlplus } from './transformer/rollup.plugin';
 
 export default {
-  input: './src/components/index.ts',
+  // input: './src/components/index.ts',
+  input: [
+    './src/components/browse/browse.tsx',
+    './src/components/sticky/sticky.tsx',
+  ],
   output: {
     format: 'esm',
     name: 'app',
-    file: 'public/dist/bundle.js',
-    // dir: 'public/dist',
+    // file: 'public/dist/bundle.js',
+    dir: 'public/dist',
   },
   plugins: [
-    HTMLPLUSDOCS({
+    htmlplus({
       prefix: 'plus',
-      docs: 'dist/json/docs.json',
-      vscode: 'dist/json/html.html-data.json',
+      docs: {
+        docs: 'json/docs.json',
+        vscode: 'json/html.html-data.json',
+      },
+      scss: {
+        includePaths: [
+          'src/styles'
+        ],
+        injectGlobalPaths: [
+          'src/styles/mixins/index.scss',
+          'src/styles/variables/index.scss'
+        ]
+      },
+      outputs: [
+        {
+          type: 'type',
+          dist: 'types',
+        },
+        {
+          type: 'docs',
+          dist: 'json/docs.json',
+        },
+        {
+          type: 'vscode',
+          dist: 'json/html.html-data.json',
+        },
+      ]
     }),
-    // HTMLPLUS({
-    //   prefix: 'plus', // TODO
-    //   scss: {
-    //     includePaths: [
-    //       'src/styles'
-    //     ],
-    //     injectGlobalPaths: [
-    //       'src/styles/mixins/index.scss',
-    //       'src/styles/variables/index.scss'
-    //     ]
-    //   }
-    // }),
     typescript(),
     resolve(),
     commonjs(),
--- packages/core.new/src/components/index.ts
+++ packages/core.new/src/components/index.ts
@@ -1,4 +1,4 @@
 // export * from './aspect-ratio/aspect-ratio';
-// export * from './browse/browse';
+export * from './browse/browse';
 // export * from './scroll-indicator/scroll-indicator';
 export * from './sticky/sticky';
