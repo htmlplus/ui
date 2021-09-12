0bd29ef4d708211e4d4328b9d532602e08d532c1
Masood Abdolian <m.abdolian@gmail.com>
2021-09-12 21:30:15 +0430

    init docs

--- packages/core.new/rollup.config.js
+++ packages/core.new/rollup.config.js
@@ -3,6 +3,7 @@ import typescript from '@rollup/plugin-typescript';
 import commonjs from 'rollup-plugin-commonjs';
 // import { terser } from 'rollup-plugin-terser';
 import { HTMLPLUS } from './transformer/rollup.plugin';
+import { HTMLPLUSDOCS } from './transformer/rollup.plugin.docs';
 
 export default {
   input: './src/components/index.ts',
@@ -13,18 +14,23 @@ export default {
     // dir: 'public/dist',
   },
   plugins: [
-    HTMLPLUS({
-      prefix: 'plus', // TODO
-      scss: {
-        includePaths: [
-          'src/styles'
-        ],
-        injectGlobalPaths: [
-          'src/styles/mixins/index.scss',
-          'src/styles/variables/index.scss'
-        ]
-      }
+    HTMLPLUSDOCS({
+      prefix: 'plus',
+      docs: 'dist/json/docs.json',
+      vscode: 'dist/json/html.html-data.json',
     }),
+    // HTMLPLUS({
+    //   prefix: 'plus', // TODO
+    //   scss: {
+    //     includePaths: [
+    //       'src/styles'
+    //     ],
+    //     injectGlobalPaths: [
+    //       'src/styles/mixins/index.scss',
+    //       'src/styles/variables/index.scss'
+    //     ]
+    //   }
+    // }),
     typescript(),
     resolve(),
     commonjs(),
