435ab917b6ee709f4a4531be654923326e05b059
Masood Abdolian <m.abdolian@gmail.com>
2022-12-15 15:28:11 +0330

    updated intersection

--- src/components/intersection/intersection.tsx
+++ src/components/intersection/intersection.tsx
@@ -1,59 +1,55 @@
 import { Attributes, Bind, Element, Event, EventEmitter, Property, State, Watch } from '@htmlplus/element';
 import * as Helpers from '@app/helpers';
-import { IntersectionBehavior } from './intersection.types';
 
 /**
+ * @stable
  * @slot default - The default slot.
  */
 @Element()
 export class Intersection {
-
   /**
-   * It specifies how intersection behaves with its children. 
-   */
-  @Property()
-  behavior?: IntersectionBehavior = 'normal';
-
-  /**
-   * Disables the intersection's trigger.
+   * Disables the component's functionality.
    */
   @Property()
   disabled?: boolean;
 
   /**
-   * It causes the callback to be called just once for the first time.
+   * The event fires only once.
    */
   @Property()
   once?: boolean;
 
   /**
-   * The element that is used as the viewport for checking visibility of the target. Must be the ancestor of the target. 
-   * Defaults to the browser viewport if not specified or if null.
+   * The element that is used as the viewport for checking visibility of the target. Must be the 
+   * ancestor of the target. Defaults to the browser viewport if not specified or if `null`.
    */
   @Property()
   root?: Element;
 
   /**
-   * Margin around the root. Can have values similar to the CSS [margin](https://developer.mozilla.org/en-US/docs/Web/CSS/margin) property, e.g. 
-   * "10px 20px 30px 40px" (top, right, bottom, left). The values can be percentages. 
-   * This set of values serves to grow or shrink each side of the root element's bounding box before computing intersections. 
-   * Defaults to all zeros.
+   * Margin around the root. Can have values similar to the CSS 
+   * [margin](https://developer.mozilla.org/en-US/docs/Web/CSS/margin) 
+   * property, e.g. `"10px 20px 30px 40px"` (top, right, bottom, left). The values can be 
+   * percentages. This set of values serves to grow or shrink each side of the root element's 
+   * bounding box before computing intersections. Defaults to all zeros.
    */
   @Property()
   rootMargin?: string;
 
   /**
-   * Either a single number or an array of numbers which indicate at what percentage of the target's visibility the observer's callback should be executed. 
-   * If you only want to detect when visibility passes the 50% mark, you can use a value of 0.5. 
-   * If you want the callback to run every time visibility passes another 25%, you would specify the array [0, 0.25, 0.5, 0.75, 1]. 
-   * The default is 0 (meaning as soon as even one pixel is visible, the callback will be run). 
-   * A value of 1.0 means that the threshold isn't considered passed until every pixel is visible.
+   * Either a single number or an array of numbers which indicate at what percentage of the 
+   * target's visibility the observer's callback should be executed. If you only want to detect 
+   * when visibility passes the 50% mark, you can use a value of 0.5. If you want the callback to 
+   * run every time visibility passes another 25%, you would specify the array 
+   * [0, 0.25, 0.5, 0.75, 1]. The default is 0 (meaning as soon as even one pixel is visible, the 
+   * callback will be run). A value of 1.0 means that the threshold isn't considered passed until 
+   * every pixel is visible.
    */
   @Property()
   threshold?: number | number[];
 
   /**
-   * This event is triggered when its children intersects with the viewport in either coming to the viewport or going out of it.
+   * Fires When the browser detects that the component has been unveiled or obscured.
    */
   @Event()
   plusChange!: EventEmitter<IntersectionObserverEntry>;
@@ -61,23 +57,20 @@ export class Intersection {
   @State()
   isIntersecting?: boolean;
 
-  @State()
-  isVisible?: boolean;
-
   observer?: IntersectionObserver;
 
   @Attributes()
   get attributes() {
     return {
-      intersecting: this.isIntersecting
+      state: this.isIntersecting ? 'in' : 'out'
     }
   }
 
-  get isDisconnected() {
+  get disconnected() {
     return this.once && this.isIntersecting;
   }
 
-  get option() {
+  get options() {
     return {
       root: this.root,
       rootMargin: this.rootMargin,
@@ -85,108 +78,45 @@ export class Intersection {
     }
   }
 
-  /**
-   * Internal Methods
-   */
-
   bind() {
-
-    this.observer = new IntersectionObserver(this.onIntersecting, this.option);
-
+    this.observer = new IntersectionObserver(this.onIntersecting, this.options);
     this.observer.observe(Helpers.host(this));
   }
 
   unbind() {
-
     this.observer?.disconnect();
-
     delete this.observer;
   }
 
-  rebind() {
-
-    this.unbind();
-
-    this.bind();
-  }
-
-  setVisible() {
-
-    switch (this.behavior) {
-
-      case 'appear':
-
-        this.isVisible = this.isIntersecting;
-
-        break;
-
-      case 'blink':
-
-        if (!this.isIntersecting) return;
-
-        this.isVisible = false;
-
-        requestAnimationFrame(() => this.isVisible = true);
-
-        break;
-
-      default:
-
-        this.isVisible = true;
-
-        break;
-    }
-  }
-
-  /**
-   * Watchers
-   */
-
   @Watch(['behavior', 'disabled', 'once', 'root', 'rootMargin', 'threshold'])
   watcher(next, prev, name) {
-
     switch (name) {
-
-      case 'behavior':
-
-        this.isVisible = next !== 'appear';
-
-        break;
-
       case 'disabled':
-
-        next && this.unbind();
-
-        !next && !this.isDisconnected && this.bind();
-
+        if (next) this.unbind();
+        else if (!this.disconnected) this.bind();
         break;
 
       case 'once':
-
+        if (next || this.disabled) break;
+        this.unbind();
+        this.bind();
         break;
 
       case 'root':
       case 'rootMargin':
       case 'threshold':
-
-        !this.disabled && !this.isDisconnected && this.rebind();
-
+        if (this.disabled || this.disconnected) break;
+        this.unbind();
+        this.bind();
         break;
     }
   }
 
-  /**
-   * Events handler
-   */
-
   @Bind()
   onIntersecting(entries: IntersectionObserverEntry[]) {
-
     const [entry] = entries;
 
     this.isIntersecting = entry.isIntersecting;
-
-    this.setVisible();
 
     this.plusChange(entry);
 
@@ -195,15 +125,9 @@ export class Intersection {
     this.unbind();
   }
 
-  /**
-   * Lifecycles
-   */
-
   connectedCallback() {
-
-    this.isVisible = (this.behavior !== 'appear');
-
-    !this.disabled && this.bind();
+    if (this.disabled) return;
+    this.bind();
   }
 
   disconnectedCallback() {
@@ -211,6 +135,6 @@ export class Intersection {
   }
 
   render() {
-    return this.isVisible ? <slot /> : undefined;
+    return <slot />
   }
 }
---
+++ src/components/intersection/todo.md
@@ -0,0 +1,1 @@
+- add examples for all properties
