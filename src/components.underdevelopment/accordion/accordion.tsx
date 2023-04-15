18614c118cc955f5f0921119cc098824f4adfc22
Masood Abdolian <m.abdolian@gmail.com>
2023-04-16 00:39:36 +0330

    updated

--- src/components.underdevelopment/accordion/accordion.tsx
+++ src/components.underdevelopment/accordion/accordion.tsx
@@ -212,8 +212,40 @@ export class Accordion {
             {this.summary}
           </slot>
           <span className="icon" part="icon">
-            <slot name="icon-collapse">+</slot>
-            <slot name="icon-expand">-</slot>
+            <slot name="icon">
+              {this.open != true ? null : (
+                <slot name="icon-collapse">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+                    ></path>
+                  </svg>
+                </slot>
+              )}
+              {this.open == true ? null : (
+                <slot name="icon-expand">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+                    ></path>
+                  </svg>
+                </slot>
+              )}
+            </slot>
           </span>
         </div>
         <div
