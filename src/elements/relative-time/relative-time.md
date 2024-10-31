ecf63fa99f55f4dce08591855ccaac6c3792cb31
Masood Abdolian <m.abdolian@gmail.com>
2024-10-31 08:59:03 +0330

    feat: Add the `RelativeTime` element

---
+++ src/elements/relative-time/relative-time.md
@@ -0,0 +1,39 @@
+# Relative Time
+
+Outputs a localized time phrase relative to the provided datetime.
+
+<Playground />
+
+<Usage />
+
+<Api />
+
+<GlobalConfig />
+
+<Examples />
+
+### Default
+
+<Example src="examples/default" />
+
+### Value
+
+<Example src="examples/value" />
+
+### Sync
+
+<Example src="examples/sync" />
+
+### Format
+
+<Example src="examples/format" />
+
+### Numeric
+
+<Example src="examples/numeric" />
+
+### Localization
+
+<Example src="examples/localization" />
+
+<LastModified />
---
+++ src/elements/relative-time/relative-time.tsx
@@ -0,0 +1,88 @@
+import { Element, Property, State } from '@htmlplus/element';
+
+import { PlusCore } from '@/core';
+
+import { RELATIVE_TIME_UNITS } from './relative-time.constants';
+
+@Element()
+export class RelativeTime extends PlusCore {
+  /**
+   * The format style for the relative time.
+   */
+  @Property()
+  format?: 'long' | 'short' | 'narrow' = 'long';
+
+  /**
+   * Determines whether the numeric value should always be displayed.
+   */
+  @Property()
+  numeric?: 'always' | 'auto' = 'auto';
+
+  /**
+   * Determines whether the relative time should be updated automatically.
+   */
+  @Property()
+  sync?: boolean = false;
+
+  /**
+   * The date and time to be displayed in a relative format.
+   */
+  @Property()
+  value?: Date = new Date();
+
+  @State()
+  formatted?: string;
+
+  @State()
+  tick?: number;
+
+  timeout: number;
+
+  get isValid() {
+    return this.value instanceof Date;
+  }
+
+  get lang() {
+    return `${this.$host.lang || window.document.documentElement.lang || window.navigator.language}`.toLowerCase();
+  }
+
+  refresh() {
+    clearTimeout(this.timeout);
+
+    if (!this.isValid) return;
+
+    const difference = this.value.getTime() - Date.now();
+
+    const unit = RELATIVE_TIME_UNITS.findLast((unit, index) => {
+      return Math.floor(Math.abs(difference) / unit.value) || !index;
+    });
+
+    const formatter = new Intl.RelativeTimeFormat(this.lang, {
+      numeric: this.numeric,
+      style: this.format
+    });
+
+    this.formatted = formatter.format(Math.ceil(difference / unit.value), unit.name);
+
+    if (!this.sync) return;
+
+    const interval = (difference > 0 ? 0 : unit.value) + (difference % unit.value);
+
+    this.timeout = window.setTimeout(() => {
+      this.tick = Math.random();
+    }, interval);
+  }
+
+  disconnectedCallback() {
+    clearTimeout(this.timeout);
+  }
+
+  updateCallback() {
+    this.refresh();
+  }
+
+  render() {
+    if (!this.isValid || !this.formatted) return 'Invalid date';
+    return <time dateTime={this.value.toISOString()}>{this.formatted}</time>;
+  }
+}
