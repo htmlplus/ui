312cdea1eb5edced3b1cb50c688951f2f556ed63
Masood Abdolian <m.abdolian@gmail.com>
2025-01-29 23:49:49 +0330

    feat: Add the `Snippet` element

---
+++ src/elements/snippet/snippet.tsx
@@ -0,0 +1,47 @@
+import { Element, Property, isCSSColor } from '@htmlplus/element';
+
+import { PlusCore } from '@/core';
+import { Style } from '@/decorators';
+
+/**
+ * @part copy    - The copy element.
+ * @part default - The default element.
+ * @part pre     - The pre element.
+ * @part symbol  - The symbol element.
+ *
+ * @slot copy    - The copy slot.
+ * @slot default - The default slot.
+ */
+@Element()
+export class Snippet extends PlusCore {
+  /**
+   * Specifies the color.
+   */
+  @Property({ reflect: true })
+  color?: string;
+
+  /**
+   * Specifies the symbol.
+   */
+  @Property()
+  symbol?: string;
+
+  @Style()
+  get style() {
+    return {
+      '--plus-snippet-color': isCSSColor(this.color) ? this.color : undefined
+    };
+  }
+
+  render() {
+    return (
+      <>
+        <pre part="pre">
+          <span part="symbol">{this.symbol}</span>
+          <slot part="default" />
+        </pre>
+        <slot name="copy" part="copy" />
+      </>
+    );
+  }
+}
